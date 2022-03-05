361395c656c18c9417394eb3de69d66e973f6b7b
Mengyong Lee <meng_yong_lee@aiap.sg>
2022-03-05 17:57:24 +0800

    Add pyautogui to control keyboard

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import cv2
 import numpy as np
+import pyautogui
 import tensorflow as tf
 import tensorflow_hub as hub
+
+from enum import Enum, auto
+from collections import deque
 
 # Configs
 threshold = 0.5
@@ -12,13 +16,16 @@ movenet = model.signatures["serving_default"]
 
 # Using Webcam
 capture = cv2.VideoCapture(0)
-counter = 0
+frame_queue = deque([], maxlen=2)
+
+
+class Action(Enum):
+    JUMP = auto()
+    DUCK = auto()
+    NOTHING = auto()
+
 
 while True:
-    counter += 1
-    if counter % 2 == 0:
-        continue # skip alternate frames to speed up processing
-
     isTrue, frame = capture.read()
     frame = frame[:, 280:1000, :]
     frame = cv2.flip(frame, 1)
@@ -45,8 +52,25 @@ while True:
 
         frame = cv2.drawMarker(frame, (round(point_x), round(point_y)), (0,0,255), markerType=cv2.MARKER_STAR, 
         markerSize=20, thickness=2, line_type=cv2.LINE_AA)
+    avg_y = sum_y / num_y if num_y != 0 else 0
 
-    avg_y = sum_y / num_y if num_y !=0 else 0
+    frame_queue.append(avg_y)
+    frame_dist = frame_queue[-1] - frame_queue[0]
+    if frame_dist > 20:
+        pyautogui.press('up')
+        action="jump"
+        print("jump")
+
+    else:
+        action="do nothing"
+
+
+    cv2.putText(frame, str(int(frame_dist)), (200, 200), cv2.FONT_HERSHEY_PLAIN, 
+                2.3, (0, 255, 0), 2, cv2.LINE_AA)
+
+    cv2.putText(frame, action, (200, 400), cv2.FONT_HERSHEY_PLAIN, 
+                2.3, (0, 255, 0), 2, cv2.LINE_AA)
+
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
